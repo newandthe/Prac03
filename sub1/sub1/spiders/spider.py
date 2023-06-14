327167d086cf5523f9b299cb57e01880422b8d24
castlectry <ksturtle2@naver.com>
2023-06-14 16:28:01 +0900

    feat: 게시물 제목 순회 크롤링

--- sub1/sub1/spiders/spider.py
+++ sub1/sub1/spiders/spider.py
@@ -14,13 +14,14 @@ class QuotesSpider(scrapy.Spider):
 
     # 게시물 상세 페이지 url로 request
     def parse_start(self, response):
-        article_url = "https://www.mois.go.kr/frt/bbs/type002/commonSelectBoardArticle.do?bbsId=BBSMSTR_000000000010"
-        data_no = response.xpath('//*[@id="print_area"]/div[2]/form/ul/li[1]/a').getall()
+        article_url = "https://www.mois.go.kr"
+        article_id = response.xpath('//*[@id="print_area"]/div[2]/form/ul/li/a/@href').getall()
+        for d in article_id:
+            url = article_url + d
+            yield scrapy.Request(url, self.parse_article)
 
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-
-    # 상게 게시물에서 정보 크롤링 시작.
+    # 게시물에서 정보 크롤링
+    def parse_article(self, response):
+        item = items.Sub1Item()
+        item['title'] = response.xpath('//*[@id="print_area"]/form/div[1]/h4/text()').get()
+        yield item
