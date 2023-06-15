cf037224f29ac0cb45119e5afc0a6ba8dce5b702
castlectry <ksturtle2@naver.com>
2023-06-15 16:13:17 +0900

    feat: 크롤링한 이미지파일 게시물번호_사진번호 형태로 D:\Prac03_Download\sub01에 저장

--- sub1/sub1/spiders/spider.py
+++ sub1/sub1/spiders/spider.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy import Spider
-
 from .. import items
 
 from urllib.parse import urljoin
@@ -12,9 +11,15 @@ class QuotesSpider(scrapy.Spider):
     # 스파이더 이름(실행 시)
     name = "sub01"
 
+    custom_settings = {
+        'ITEM_PIPELINES': {'sub1.pipelines.CustomImagesPipeline': 1}
+    }
+
     def start_requests(self):
-        url = 'https://www.mois.go.kr/frt/bbs/type002/commonSelectBoardList.do?bbsId=BBSMSTR_000000000010'
-        yield scrapy.Request(url, self.parse_start)
+        url = 'https://www.mois.go.kr/frt/bbs/type002/commonSelectBoardList.do?bbsId=BBSMSTR_000000000010&searchCnd=&searchWrd=&pageIndex=%s' # %s
+        start_page = 1  # start page 정의
+        for i in range(3): # 1부터 3번 페이지까지 (i는 0부터 시작)
+            yield scrapy.Request(url % (i + start_page), self.parse_start)
 
     # 게시물 상세 페이지 url로 request
     def parse_start(self, response):
@@ -56,13 +61,27 @@ class QuotesSpider(scrapy.Spider):
 
         item['readcount'] = readcount
 
-        imgnum_list = response.xpath('//*[@id="print_area"]/form/div[1]/div[3]/div/ul/li/p/a/@href').getall()
-        file_sn_list = [re.search(r'fileSn=(\d+)', img_url).group(1) for img_url in imgnum_list]
-        item['imgnum'] = file_sn_list
+        rel_img_urls = response.xpath('//*[@id="print_area"]/form/div[1]/div[3]/div/ul/li/p/a/@href').getall()
+
+        imgsrc_list = response.xpath('//*[@id="print_area"]/form/div[1]/div[3]/div/ul/li/p/a/@href').getall()
+
+        item['imgsrc'] = [urljoin('https://www.mois.go.kr/', path) for path in imgsrc_list]
 
         content_list = response.xpath('//*[@id="print_area"]/form/div[1]/div[3]/div/ul/li/p/text()').getall()
         item['contentnum'] = [content.strip() for content in content_list if content.strip()]
 
         imgpath_list = response.xpath('//*[@id="print_area"]/form/div[1]/dl[1]/dd/div/ul/li/a[1]/@href').getall()
         item['imgpath'] = [urljoin('https://www.mois.go.kr/', path) for path in imgpath_list]
+
+        item['image_urls'] = self.url_join(imgpath_list, response)
+
+        # print(item)
         yield item
+
+    def url_join(self, rel_img_urls, response):
+        joined_urls = []
+        for rel_img_url in rel_img_urls:
+            joined_urls.append(response.urljoin(rel_img_url))
+
+        return joined_urls
+
