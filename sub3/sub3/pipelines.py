200759a970024f04d5983b98068de75a5fa1742e
newandthe <ksturtle2@naver.com>
2023-06-19 02:17:23 +0900

    fix : sub03 크롤링 기본틀 잡기

--- sub3/sub3/pipelines.py
+++ sub3/sub3/pipelines.py
@@ -8,6 +8,25 @@
 from itemadapter import ItemAdapter
 
 
-class Sub3Pipeline:
-    def process_item(self, item, spider):
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+from itemadapter import ItemAdapter
+
+
+class Sub3Pipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item.get('image_urls', []):
+            yield scrapy.Request(image_url, meta={'item': item})
+
+    def file_path(self, request, response=None, info=None):
+        item = request.meta['item']
+        ntt_id = item['ntt_id']
+        image_guid = request.url.split('/')[-1]
+        filename = f'nttId_{ntt_id}_{image_guid}'
+        return filename
+
+    def item_completed(self, results, item, info):
+        adapter = ItemAdapter(item)
+        images = [x for ok, x in results if ok]
+        adapter['images'] = images
         return item
--- sub3/sub3/spiders/spider.py
+++ sub3/sub3/spiders/spider.py
@@ -1,16 +1,36 @@
 import scrapy
+from .. import items
 
-
-class QuotesSpider(scrapy.Spider):
+class NewsSpider(scrapy.Spider):
     name = "sub03"
-
-    firsturl = "https://www.yna.co.kr/"
-    middleurl = "/all?site=navi_"
-    # lasturl = "_depth02"
+    start_page = 1  # 크롤링 시작 페이지
+    end_page = 2  # 크롤링 끝 페이지
 
     def start_requests(self):
-        firsturl = "https://www.yna.co.kr/"
+        urls = [
+            f"https://www.yna.co.kr/politics/all/{page}" for page in range(self.start_page, self.end_page + 1)
+        ]
+        # urls += [
+        #     f"https://www.yna.co.kr/economy/all/{page}" for page in range(self.start_page, self.end_page + 1)
+        # ]
+        #
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
+    def parse(self, response):
+        # 현재 페이지에서 기사 URL 추출
+        article_links = response.xpath(
+            '//*[@id="container"]/div/div/div[1]/section/div[1]/ul/li/div/div[2]/a/@href').getall()
+        for article_link in article_links:
+            yield response.follow(article_link, callback=self.parse_article)
 
-    def parse_start(self, response):
-        article_url = ""
+    def parse_article(self, response):
+        item = items.Sub3Item()
+
+        item['title'] = response.css("header h1::text").get()
+        item['author'] = response.css(".tit-name::text").get()
+        date_xpath = "//span[@class='update-time']/text()"
+        item['date'] = response.xpath("//p[@class='update-time']/text()[normalize-space()]").get()
+        item['date'] = item['date'].strip() if item['date'] else None
+        item['content'] = response.css(".story-news p::text").getall()
+        yield item
